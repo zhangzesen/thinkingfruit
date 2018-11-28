fab21a21a4282e66fc43851609ecf2b9fc1c8878
zhangzesen <3151059861@qq.com>
2018-11-28 14:19:48 +0800

    代码微调

--- thinkingfruit/thinkingFruit-client/src/main/java/com/thinkingFruit/client/controller/HomePageController.java
+++ thinkingfruit/thinkingFruit-client/src/main/java/com/thinkingFruit/client/controller/HomePageController.java
@@ -1,23 +1,13 @@
 package com.thinkingFruit.client.controller;
 
-import java.util.List;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
-
-import com.thinkingFruit.admin.entity.Commodity;
-import com.thinkingFruit.admin.service.CommodityService;
-import com.ysdevelop.common.result.Result;
 
 @Controller
 @RequestMapping(value="/home")
 public class HomePageController {
-	
-	@Autowired
-	CommodityService commodityService;
+
 	/**
 	 *首页跳转
 	 * */
@@ -26,13 +16,5 @@ public class HomePageController {
 		return "home/index";
 	}
 	
-	/**
-	 * 获取index的商品列表
-	 * */
-	@RequestMapping(value = "/list", produces = "application/json;charset=utf-8")
-	@ResponseBody
-	public Result<List<Commodity>> list() {
-		List<Commodity> clientListCommodity = commodityService.clientListCommodity();
-		return Result.successData(clientListCommodity);
-	}
+
 }
