1549e35e4af2c00b3e27d22bfb062a915bc44bb0
zhangzesen <3151059861@qq.com>
2019-01-16 14:16:45 +0800

    代码微调

--- thinkingfruit/thinkingFruit-admin/src/main/webapp/static/js/purchaseOrder/checkIndex.js
+++ thinkingfruit/thinkingFruit-admin/src/main/webapp/static/js/purchaseOrder/checkIndex.js
@@ -94,7 +94,7 @@ var purchaseOrder_checkIndex_ops = {
 	                       {field: 'id', title: '编号',align: 'center', width:'5%'}
 				           , {field: 'orderNo', title: '订单号', width:'17%',align: 'center'}
 				           , {field: 'orderMemberName', title: '代理人', width:'8%',align: 'center'}
-					       , {field: 'certificateImage', width:'15%' , style:'height:50;',title: '凭证图片', align: 'center',templet:'<div><img style="height:50;width:100%" src="'+WEB_ROOT+'{{d.certificateImage}}'+'"></div>'}
+					       , {field: 'certificateImage', width:'15%' , style:'height:50;',title: '凭证图片', align: 'center',templet:'<div><img style="height:50px;width:100%" src="'+WEB_ROOT+'{{d.certificateImage}}'+'"></div>'}
 				           , {field: 'checkStatus', title: '订单审核状态', width:'8%',align: 'center'}
 				           , {field: 'commodityName', title: '商品名', width:'8%',align: 'center'}
 				           , {field: 'commodityCount', title: '商品数量', width:'8%',align: 'center'}
